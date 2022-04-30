4c0334797092a075db4ca508313ad127ff9486f4
shuhaibzahir <yesshuhaib.ss@gmail.com>
2022-04-30 14:42:02 +0530

    postrouter implimented

--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const {randomBytes} = require('crypto')
 const app = express()
 
 app.use(express.urlencoded({extended:false}))
+const posts ={}
+
+app.get("/posts",(req,res)=>{
+    res.send(posts)
+})
+
+app.get("/posts/:id",(req,res)=>{
+    const {id} =req.params
+})
 
 app.post('/posts',(req,res)=>{
-    
+    console.log(req.body)
+   const id =  randomBytes(4).toString('hex');
+    posts[id]={
+        id,
+        title:req.body.title
+    }
+    res.status(201).send(posts[id])
 })
 
 
